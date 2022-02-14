89ab47747d8b8f338ab77cbd8795fec54b13e212
Shaw <shawwian@gmail.com>
2022-02-14 18:07:16 -0500

    Windows build: copy assets into build dir

--- build.bat
+++ build.bat
@@ -3,6 +3,7 @@ mkdir .\build-windows
 pushd .\build-windows
 
 copy ..\SDL2\bin\SDL2.dll .
+xcopy /E /I /Y ..\assets .\assets
 
 setlocal EnableDelayedExpansion
 setlocal EnableExtensions
@@ -10,5 +11,7 @@ set sources=
 for %%i in (..\source\*) do set sources=!sources! %%i
 
 gcc -Wall -g -std=gnu11 %sources% -I..\SDL2\include -I..\include -L..\SDL2\lib -lmingw32 -lSDL2main -lSDL2 -o 2pacman
+
+2pacman
 
 popd
